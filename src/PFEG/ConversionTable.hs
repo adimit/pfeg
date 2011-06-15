ca9ac18e3ecf0e5d886807c3b9585d6a695a9113
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2011-06-15 10:07:53 +0200

    Filled in the stubs. I'd like a caching mechanism
    … and endless cheap petroleum, world peace, a cake and a million Euro.

--- src/PFEG/ConversionTable.hs
+++ src/PFEG/ConversionTable.hs
@@ -1,9 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
 module PFEG.ConversionTable where
 
 import Database.HDBC
-import Database.HDBC.Sqlite3
 
-import PFEG.BinaryMagic
 import PFEG.Common
 import PFEG.Types
 
@@ -12,19 +11,33 @@ import qualified Data.ByteString.Lazy as S
 
 import Data.Int
 
+-- Just for a type signature
+import Data.Convertible.Base (Convertible)
+
 -- Convenient IO wrappers around SQL statements.
 
 sqlIndex2Form, sqlForm2Index :: String
 sqlIndex2Form = "SELECT form FROM " ++ (unigramTable standardConfig) ++ " WHERE id==?"
 sqlForm2Index = "SELECT id FROM " ++ (unigramTable standardConfig) ++ " WHERE form==?"
 
-convertValue :: String -> Connection -> SqlValue -> IO SqlValue
-convertValue sql conn = fmap (head.concat).quickQuery conn sql . (:[])
+convertValue :: String -> TableAccess -> SqlValue -> IO SqlValue
+convertValue sql acc = fmap (head.concat).quickQuery (connection acc) sql . (:[])
 
-indexContext :: Connection -> Context S.ByteString -> IO (Context Int32)
-indexContext f c = case c of 
-    (Context3 a1 a2 a3 a4 a5 a6) -> undefined
+indexContext :: TableAccess -> Context S.ByteString -> IO (Context Int32)
+indexContext acc  = iX acc sqlForm2Index
 
-unindexContext :: Connection -> Context Int32 -> IO (Context L.ByteString)
-unindexContext f c = case c of
-    (Context3 a1 a2 a3 a4 a5 a6) -> undefined-}
+unindexContext :: TableAccess -> Context Int32 -> IO (Context L.ByteString)
+unindexContext acc = iX acc sqlIndex2Form
+
+-- | Helper function for (un)indexContext, that wraps around the unweildy Context
+-- constructors.
+iX :: (Convertible a SqlValue, Convertible SqlValue a1) =>
+      TableAccess -> String -> Context a -> IO (Context a1)
+iX acc sql c = case c of
+    (Context3 a1 a2 a3 a4 a5 a6) -> do (a1':a2':a3':a4':a5':a6':[]) <- f [a1,a2,a3,a4,a5,a6]
+                                       return $ Context3 a1' a2' a3' a4' a5' a6'
+    (Context2    a2 a3 a4 a5   ) -> do (    a2':a3':a4':a5'    :[]) <- f [   a2,a3,a4,a5   ]
+                                       return $ Context2     a2' a3' a4' a5'
+    (Context1       a3 a4      ) -> do (        a3':a4'        :[]) <- f [      a3,a4      ]
+                                       return $ Context1         a3' a4'
+    where f = (mapM (fmap fromSql.convertValue sql acc.toSql))
