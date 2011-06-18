3ce499381bfff27471d2434541a5e5b54feb3a98
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2011-06-19 01:50:59 +0200

    Moved sentenceP, wordP to PFEG.Common
    This can be made use of in other modules as well.

--- src/PFEG/Common.hs
+++ src/PFEG/Common.hs
@@ -7,12 +7,21 @@ module PFEG.Common
       -- * Measuring execution times
     , doTimed
     , doTimed_
+      -- * Attoparsec parsers for the TT-style corpora
+    , wordP
+    , sentenceP
     ) where
 
 import PFEG.Types
 import Data.Time.Clock
 import qualified Data.HashMap.Strict as T
 import qualified Data.Text as X
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+
+import Data.Attoparsec
+import Control.Applicative hiding (many)
+import Data.Word (Word8)
 
 standardConfig :: Configuration
 standardConfig = Config { lemmaTable   = "lemmas"
@@ -30,3 +39,25 @@ doTimed f = do
 
 doTimed_ :: IO a -> IO NominalDiffTime
 doTimed_ f = fmap snd (doTimed f)
+{-# INLINE doTimed_ #-}
+
+wordP :: Parser (Word Text)
+wordP = do surface <- takeTill (==tab8)
+           tag     <- skip (==tab8) *> takeTill (==tab8)
+           lemma   <- skip (==tab8) *> takeTill (==nl8)
+           skip (==nl8)
+           return $ ( X.toCaseFold.decodeUtf8 $ surface
+                    ,              decodeUtf8   tag
+                    , X.toCaseFold.decodeUtf8 $ lemma)
+
+sentenceP :: Parser (Sentence Text)
+sentenceP = do words <- wordP `manyTill` (word8 nl8) <* skipWhile (==nl8)
+               return words
+
+c28 :: Char -> Word8
+c28 = fromIntegral.fromEnum
+{-# INLINE c28 #-}
+
+nl8, tab8 :: Word8
+nl8  = c28 $! '\n'
+tab8 = c28 $! '\t'
--- src/tools/recordContexts.hs
+++ src/tools/recordContexts.hs
@@ -37,32 +37,13 @@ establishConnections unigramT contextT = do
                           , table = "contexts" }
     return (unigramA,contextA)
 
-wordP :: Parser (Word Text)
-wordP = do surface <- takeTill (==tab8)
-           tag     <- skip (==tab8) *> takeTill (==tab8)
-           lemma   <- skip (==tab8) *> takeTill (==nl8)
-           skip (==nl8)
-           return $ ( T.toCaseFold.decodeUtf8 $ surface
-                    ,              decodeUtf8   tag
-                    , T.toCaseFold.decodeUtf8 $ lemma)
-
-sentenceP :: Parser (Sentence Text)
-sentenceP = do words <- wordP `manyTill` (word8 nl8) <* skipWhile (==nl8)
-               return words
-
 corpusI :: (Monad m) => Iteratee ByteString m [Sentence Text]
 corpusI = parserToIteratee (many sentenceP)
+{-# INLINE corpusI #-}
 
 extractItems :: [Sentence Text] -> [Item Text Text]
 extractItems (s:ss) = exS s ++ extractItems ss
     where exS s' = undefined
-
-c28 :: Char -> Word8
-c28 = fromIntegral.fromEnum
-
-nl8, tab8 :: Word8
-nl8  = c28 $ '\n'
-tab8 = c28 $ '\t'
 
 main :: IO ()
 main = do
