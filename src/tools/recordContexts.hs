95cb00419fa737d6a98a8a938276fe9cd740d5f9
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2011-06-19 01:30:31 +0200

    Filled in the parsing/iteratee stub.

--- src/tools/recordContexts.hs
+++ src/tools/recordContexts.hs
@@ -7,7 +7,27 @@ import System.Environment (getArgs)
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
-establishConnections :: FilePath -> FilePath -> (TableAccess,TableAccess)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import Data.ByteString (ByteString)
+
+import Data.Iteratee.Char
+import Data.Iteratee.IO
+import Data.Iteratee.ListLike as LL
+import qualified Data.Iteratee as I
+
+import Data.Attoparsec
+import Data.Attoparsec.Iteratee
+
+import Data.Word (Word8)
+
+import qualified Data.Text as T
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+
+import Control.Applicative hiding (many)
+
+establishConnections :: FilePath -> FilePath -> IO (TableAccess,TableAccess)
 establishConnections unigramT contextT = do
     unigramC <- connectSqlite3 unigramT
     contextC <- connectSqlite3 contextT
@@ -17,6 +37,32 @@ establishConnections unigramT contextT = do
                           , table = "contexts" }
     return (unigramA,contextA)
 
+wordP :: Parser (Word Text)
+wordP = do surface <- takeTill (==tab8)
+           tag     <- skip (==tab8) *> takeTill (==tab8)
+           lemma   <- skip (==tab8) *> takeTill (==nl8)
+           skip (==nl8)
+           return $ ( T.toCaseFold.decodeUtf8 $ surface
+                    ,              decodeUtf8   tag
+                    , T.toCaseFold.decodeUtf8 $ lemma)
+
+sentenceP :: Parser (Sentence Text)
+sentenceP = do words <- wordP `manyTill` (word8 nl8) <* skipWhile (==nl8)
+               return words
+
+corpusI :: (Monad m) => Iteratee ByteString m [Sentence Text]
+corpusI = parserToIteratee (many sentenceP)
+
+extractItems :: [Sentence Text] -> [Item Text Text]
+extractItems (s:ss) = exS s ++ extractItems ss
+    where exS s' = undefined
+
+c28 :: Char -> Word8
+c28 = fromIntegral.fromEnum
+
+nl8, tab8 :: Word8
+nl8  = c28 $ '\n'
+tab8 = c28 $ '\t'
 
 main :: IO ()
 main = do
