cdb71d6e2c33781020d7e65e85734b674c862a8b
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-01-21 19:51:16 +0100

    Add IO code and imports.

--- src/tools/matchContexts.hs
+++ src/tools/matchContexts.hs
@@ -1,13 +1,15 @@
 -- {-# LANGUAGE TupleSections,BangPatterns #-}
 module Main where
 
+import Data.Time.Clock
 import Data.List (intercalate)
 import Control.Monad.Reader
 import Control.Monad.State
 import GHC.IO.Handle (Handle)
 import Data.Maybe (catMaybes)
 
-import System.IO (hPutStrLn)
+import System.Environment (getArgs)
+import System.IO (hPutStrLn,IOMode(ReadMode,AppendMode),hFileSize,withFile,openFile,hClose)
 import System.Time.Utils (renderSecs)
 
 import Data.Iteratee.IO
@@ -15,6 +17,11 @@ import Data.Iteratee (Iteratee)
 import qualified Data.Iteratee as I
 
 import Control.Concurrent.MVar
+import Control.Concurrent.Chan
+import Control.Concurrent
+import Control.Exception (bracket)
+
+import Graphics.Vty.Terminal
 
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -30,10 +37,10 @@ data LogState = LogState { currentItem :: Int }
 
 data Configuration = Configuration
     { testShard  :: Maybe Int
-    , targets    :: [Text]
     , connection :: Connection
     , logVar     :: MVar LogData
-    , sqlLogH    :: Handle }
+    , sqlLogH    :: Handle
+    , resultH    :: Handle }
 
 data MatchMode = P | L | S deriving Show
 type MatchPattern = [Maybe MatchMode]
@@ -87,7 +94,37 @@ logResult :: Handle -> LogData -> StateT LogState IO ()
 logResult _ _ = undefined
 
 main :: IO ()
-main = undefined -- (runReaderT $ I.run =<< enumFile chunk_size corpus (I.joinI $ I.convStream corpusI matchI)) cf
+main = do
+    (contextT:corpus:shard:_) <- getArgs
+    statusV <- newChan
+    resultV <- newEmptyMVar
+    startTime <- getCurrentTime
+    term <- terminal_handle
+    csize <- withFile corpus ReadMode hFileSize
+
+    let etc = (fromIntegral csize `div` chunk_size)+1
+    putStrLn $ "Estimated amount of chunks " ++ show etc
+
+    void $ forkIO $ logger etc startTime statusV
+
+    bracket (do contextdb <- connectSqlite3 contextT
+                hide_cursor term
+                sqlLogH' <- openFile "sql.log" AppendMode
+                resultH' <- openFile "result.log" AppendMode
+                return (contextdb,sqlLogH',resultH'))
+            (\(contextdb,sqlLogH',resultH') -> do
+                show_cursor term
+                disconnect contextdb
+                hClose sqlLogH'
+                hClose resultH')
+            (\(contextdb,sqlLogH',resultH') -> do
+                let cf = Configuration
+                         { testShard = Just $ read shard
+                         , connection = contextdb
+                         , logVar = resultV
+                         , sqlLogH = sqlLogH'
+                         , resultH = resultH' }
+                (runReaderT $ I.run =<< enumFile chunk_size corpus (I.joinI $ I.convStream corpusI matchI)) cf)
 
 prettyMatchMode :: [Maybe MatchMode] -> String
 prettyMatchMode = take 6.concatMap (maybe "_" show).(++ repeat Nothing)
