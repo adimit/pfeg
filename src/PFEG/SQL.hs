66ff2a395d3d441e8961a228be3e0581c5113c3c
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-01-22 00:29:26 +0100

    Chang hash table name to the new matchContext one.

--- src/PFEG/SQL.hs
+++ src/PFEG/SQL.hs
@@ -19,7 +19,7 @@ import Data.Convertible.Base (Convertible)
 import Prelude hiding (null)
 
 insertHash, updateWithHash, insertWithHash :: String
-insertHash = "INSERT OR IGNORE INTO hashes (h,"++contextNames++") VALUES (?,"++ questionmarks 18++")"
+insertHash = "INSERT OR IGNORE INTO hash (h,"++contextNames++") VALUES (?,"++ questionmarks 18++")"
     where contextNames = intercalate "," $ map (`commas` [1..6]) "slp"
 updateWithHash = "UPDATE ctxt SET c=c+1 WHERE h==? AND i==? AND t==?"
 insertWithHash = "INSERT INTO ctxt (h,i,t,c) VALUES (?,?,?,1)"
