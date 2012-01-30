87a91bef9cea263546cb4f6e26a122936a1968e5
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-01-30 13:34:17 +0100

    Remove old cruft.

--- src/PFEG/Types.hs
+++ src/PFEG/Types.hs
@@ -3,17 +3,11 @@ module PFEG.Types
     , Word
     , Sentence
     , Table
-    , TableAccess(..)
-    , Target
     , ID
     ) where
 
 import qualified Data.Text as X
 import qualified Data.HashMap.Strict as T
-import Database.HDBC.Sqlite3
-import Data.Text (Text)
-
-data TableAccess = Access { connection :: Connection, table :: Table }
 
 type Wordcounts = T.HashMap X.Text Int
 type Word a = (a,a,a)
@@ -22,4 +16,3 @@ type Sentence a = [Word a]
 type Table = String
 
 type ID = Int -- SQL row IDs
-type Target = Text
