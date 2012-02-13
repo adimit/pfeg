768f37fd1dfa0248dd9e91e74205314c8437aa59
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-02-14 00:23:48 +0100

    stub: prepare pfeg for new configuration method.

--- src/tools/pfeg.hs
+++ src/tools/pfeg.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns,DeriveDataTypeable #-}
+{-# LANGUAGE ScopedTypeVariables,BangPatterns,DeriveDataTypeable #-}
 module Main where
 
 import PFEG.SQL
@@ -42,102 +42,23 @@ import Control.Monad (forever,liftM,foldM,when,void)
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
-import System.Console.CmdArgs
-
 import Graphics.Vty.Terminal
 
+import PFEG.Configuration
+import ReadArgs
+
 type PFEG a = ReaderT PFEGConfig IO a
-data PFEGConfig = PFEGConfig
-    { cUnigramIds :: UnigramIDs
-    , cTargetIds  :: IntMap Text
-    , cDatabase :: Connection
-    , cLogVar  :: MVar LogData
-    , cLogFile :: Handle }
 
 data LogData = LogData
     { logItem    :: Item Text
     , logResults :: [(MatchPattern,Result)] }
 
-{- START CMDARGS GIBBERISH -}
-
-data PFEGMain = Record { corpus    :: FilePath
-                       , unigrams  :: FilePath
-                       , database  :: FilePath
-                       , sqlLog    :: Maybe FilePath
-                       , targets   :: String
-                       , resultLog :: FilePath }
-              | Match  { corpus    :: FilePath
-                       , unigrams  :: FilePath
-                       , database  :: FilePath
-                       , sqlLog    :: Maybe FilePath
-                       , targets   :: String
-                       , resultLog :: FilePath }
-              deriving (Data, Typeable)
-
-instance Show PFEGMain where
-    show (Record c u db sql ts _r) =
-        standardMessage "Recording to" c u db sql ts ""
-    show (Match  c u db sql ts r) =
-        standardMessage "Matching aigainst" c u db sql ts ("Logging result to '" ++ r ++ "'")
-
-standardMessage :: String -> FilePath -> FilePath -> FilePath ->
-                  Maybe FilePath -> String -> String -> String
-standardMessage m c u db sql ts r = m ++ " '" ++ db ++ "'\n" ++
-                                      "from corpus '" ++ c ++ "'.\n" ++
-                                      "unigrams are '" ++ u ++ "'\n" ++ r ++ "\n" ++
-                                      "targets are '" ++ show ts ++ "'\n" ++
-                                      case sql of
-                                           Nothing  -> ""
-                                           (Just x) -> "Logging SQL to '" ++ x ++ "'.\n"
-
-recordCmd, matchCmd :: PFEGMain
-recordCmd = Record { corpus    = commonCorpus def
-                   , unigrams  = commonUnigrams "../db/de/uni.db"
-                   , database  = commonDatabase "../db/de/ctx.db"
-                   , sqlLog    = commonSqlLog def
-                   , targets   = commonTargets
-                   , resultLog = def &= typ "FILE" &= help "Log results to"}
-                              &= help "Learn contexts from corpus and store in DB."
-
-matchCmd  = Match  { corpus    = commonCorpus def
-                   , unigrams  = commonUnigrams "../db/de/uni.db"
-                   , database  = commonDatabase "../db/de/ctx.db"
-                   , sqlLog    = commonSqlLog def
-                   , targets   = commonTargets
-                   , resultLog = "result.log" &= typ "FILE" &= help "Result log file location."}
-                              &= help "Match context from corpus against DB."
-
-commonTargets = "in,von,mit,für,im,auf,nach,an,aus,am"
-    &= typ "TARGETLIST" &= help "Comma-separated list of targets, e.g 'in,von,mit…'"
-
-commonCorpus, commonUnigrams, commonDatabase, commonSqlLog :: Data v => v -> v
-commonCorpus   x = x &= typ "FILE" &= help "Input corpus in TT format."
-commonUnigrams x = x &= typ "FILE" &= help "Location of the unigram index database."
-commonDatabase x = x &= typ "FILE" &= help "Location of the Context database."
-commonSqlLog   x = x &= typ "FILE" &= help "Output of SQL log, if desired."
-
-mode :: Mode (CmdArgs PFEGMain)
-mode = cmdArgsMode $ modes [matchCmd,recordCmd]
-    &= help "Predicting Functional Elements in German (and others)"
-    &= program "pfeg"
-    &= summary "PFEG 0.1, Aleksandar Dimitrov 2012"
-
 main :: IO ()
-main = (\m -> print m >> handle m) =<< cmdArgsRun mode
-
-{- END CMDARGS GIBBERISH -}
-
-cacheHash :: Statement -> StateT UnigramIDs IO ()
-cacheHash s = liftIO (void $ execute s []) >> fetchAll
-    where fetchAll = do
-          row <- liftIO $ fetchRow s
-          case row of
-               Nothing       -> return ()
-               Just (f:i:[]) -> do
-                   m <- get
-                   put $! M.insert (fromSql f) (fromSql i) m
-                   fetchAll
-               _             -> fail "Malformed result in unigrams."
+main = do
+    (mode' :: String, configFile :: FilePath) <- readArgs
+    let mode = mode' == "match"
+    putStrLn "Initializing…"
+    handle mode configFile
 
 data SQL = RecordSQL { updateTarget  :: Statement
                      , insertContext :: Statement
@@ -160,32 +81,20 @@ logResult h resV = forever log -- who wants to be forever log
                    liftIO $ takeMVar resV >>= hPutStr h.((show n ++ ": ")++).show >> hFlush h
                    put (LogState $ n+1)
 
--- Mode-independent common initialization and cleanup code
-handle :: PFEGMain -> IO ()
-handle m =
-    bracket (do putStrLn "Initializing."
-                sv   <- newEmptyMVar
-                cdb  <- connectSqlite3 (database m)
-                cu   <- connectSqlite3 (unigrams m)
-                s    <- prepare cu "SELECT f,id FROM unigrams"
-                uids <- execStateT (cacheHash s) M.empty
-                disconnect cu
-                putStrLn "Done."
+handle :: Bool -> FilePath -> IO ()
+handle mode configFile =
+    bracket (do putStrLn "Initializing…"
                 terminal_handle >>= hide_cursor
-                lf <- openFile (resultLog m) AppendMode
-                let targets' = map (T.strip . T.pack) $ splitOn "," (targets m)
-                return PFEGConfig { cUnigramIds = uids
-                                    , cDatabase   = cdb
-                                    , cTargetIds  = mkTargetIds targets' uids
-                                    , cLogVar     = sv
-                                    , cLogFile    = lf })
+                configAttempt <- configurePFEG mode configFile
+                case configAttempt of
+                     (Left err) -> error $ "Initialization failed: "++ show err
+                     (Right config) -> return config)
             (\session -> do
+                putStrLn "Shutting down…"
                 terminal_handle >>= show_cursor
-                putStrLn "Disconnecting…"
-                hClose (cLogFile session)
-                disconnect (cDatabase session))
-            (\session -> do
-                logVar <- newChan
+                deinitialize session)
+            (\session -> undefined)
+             {- logVar <- newChan
                 t0     <- getCurrentTime
                 csize  <- withFile (corpus m) ReadMode hFileSize
                 void $ forkIO $ logger ((fromIntegral csize `div` chunk_size)+1) t0 logVar
@@ -198,13 +107,24 @@ handle m =
                         , I.joinI $ I.convStream corpusI (I.mapChunksM_ $ mapM processor.getItems targets')])
                 runReaderT iteratee session
                 putStr "\nCommitting…"
-                doTimed_ (commit $ cDatabase session) >>= putStrLn.("\rCommitted in "++).renderSecs.round)
-
-mkTargetIds :: [Text] -> UnigramIDs -> IntMap Text
-mkTargetIds ts uids = IM.fromList $ zip (mapMaybe (`M.lookup` uids) ts) ts
+                doTimed_ (commit $ cDatabase session) >>= putStrLn.("\rCommitted in "++).renderSecs.round) -}
 
 type ItemProcessor = Item Text -> PFEG ()
 
+prepareProcessor :: PFEGConfig -> IO ItemProcessor
+prepareProcessor session =
+    case pfegMode session of
+         (Record _) -> do
+                      insertCtxtS <- prepare (contextDB session) insertCtxtSQL
+                      insertTrgtS <- prepare (contextDB session) insertTargetSQL
+                      updateS     <- prepare (contextDB session) updateSQL
+                      let sql = RecordSQL { updateTarget  = updateS
+                                          , insertContext = insertCtxtS
+                                          , insertTarget  = insertTrgtS }
+                      return $ recordF sql
+         (Match _ _ _) -> undefined
+
+{-
 -- Prepare SQL, assemble the @ItemProcessor@ depending on which mode we're in.
 prepareProcessor :: PFEGMain -> PFEGConfig -> IO ItemProcessor
 prepareProcessor (Record _ _  _ _ _ _) session = do
@@ -218,32 +138,35 @@ prepareProcessor (Record _ _  _ _ _ _) session = do
 prepareProcessor (Match _ _ _ _ _ _) session = do
     sql <- precompileSQL mkMatchSQL (cDatabase session) matchmodes
     return $ matchF sql
+-}
 
 matchF :: MatcherInit -> Item Text -> PFEG ()
 matchF sql i = do
     cf <- ask
     results <- mapM (matchAPattern sql i) matchmodes
-    liftIO $ putMVar (cLogVar cf) (LogData i (zip matchmodes results))
+    liftIO undefined -- putMVar (cLogVar cf) (LogData i (zip matchmodes results))
 
 -- given a pattern, matcherInit and an Item, give a result from the database
 -- helper function for @matchF@.
 matchAPattern :: MatcherInit -> Item Text -> MatchPattern -> PFEG Result
 matchAPattern sql i mm = do
     cf <- ask
-    let pattern = item2SQLp mm (indexItem (cUnigramIds cf) i)
+    let matchMode = case pfegMode cf of (Record _) -> error "matchAPattern only works in match mode."
+                                        x          -> x
+    let pattern = item2SQLp mm (indexItem (unigramID cf) i)
     case mm `M.lookup` sql of
          Nothing -> error "IMPOSSIBRU!"
          (Just s) -> do
              void $ liftIO $ execute s pattern
              rows <- liftIO $ fetchAllRows' s
              return $ foldl f [] rows
-             where f r (t:c:idc:[]) = (cTargetIds cf IM.! fromSql t,fromSql c,fromSql idc):r
+             where f r (t:c:idc:[]) = (targetIDs matchMode IM.! fromSql t,fromSql c,fromSql idc):r
                    f _ xs           = error $ "Unexpected data format." ++ show xs
 
 recordF :: SQL -> Item Text -> PFEG ()
 recordF sql i = do
     cf <- ask
-    let item'    = indexItem (cUnigramIds cf) i
+    let item'    = indexItem (unigramID cf) i
         pattern  = item2SQL item' -- just the slp forms
         pattern' = toSql (target item'):pattern -- the slp forms with target prepended
     numRows <- liftIO $ execute (updateTarget sql) pattern'
