ae3e0976d66437857ed3ea82d96061895f0b429b
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-02-29 11:30:50 +0100

    Adapt SQL.hs to new query function.

--- src/PFEG/SQL.hs
+++ src/PFEG/SQL.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators, FlexibleContexts #-}
 module PFEG.SQL
     ( item2SQL
     , item2postgresArrays
@@ -16,10 +16,21 @@ import PFEG.Context
 import Data.Text (Text)
 import qualified Data.Text as T
 
-import Data.List (intercalate)
+import Data.List (mapAccumL,intercalate)
 import Data.Maybe (catMaybes)
 
 import Prelude hiding (null)
+
+-- fiendishly ugly, but it gets the job done.
+groupByJust :: [Maybe a] -> [[a]]
+groupByJust l = let (rest,list) = mapAccumL f [] l
+                in  catMaybes list ++ case [catMaybes rest] of
+                                          [[]] -> []
+                                          xs -> xs
+                where f :: [Maybe a] -> Maybe a -> ([Maybe a],Maybe [a])
+                      f [] Nothing = ([],Nothing)
+                      f acc Nothing = ([],Just $ catMaybes acc)
+                      f acc a@Just{} = (acc++[a],Nothing)
 
 -- | Call records_upsert(TEXT,INTEGER,TEXT[])
 upsertRecord :: String
@@ -31,27 +42,35 @@ upsertUnigram = "SELECT unigram_upsert(?,?)"
 
 -- | Call match_function(INTEGER[],TEXT[])
 queryDatabase :: String
-queryDatabase = "SELECT match_function(?,?)"
+queryDatabase = "SELECT query_records(?,?)"
 
-item2SQLp :: MatchPattern -> Item i -> [i]
-item2SQLp (MatchPattern mm) Item{ pItem = Context pI, lItem = Context lI, sItem = Context sI}  =
-    catMaybes $ zipWith ms mm (zip3 pI lI sI)
-    where ms (Just P) = Just . fst3
-          ms (Just L) = Just . snd3
-          ms (Just S) = Just . trd3
-          ms Nothing  = const Nothing
+item2SQLp :: MatchPattern -> Item i -> [[i]]
+item2SQLp mm Item{ pItem = Context pI, lItem = Context lI, sItem = Context sI}  =
+    groupByJust $ zipWith ($) (matchPatternToPositional mm) (sI++lI++pI)
 
 -- | Prepare an item and a certain matchmode for @queryDatabase@
 item2postgresArrays :: (a -> String) -> Item a -> MatchPattern -> (SqlValue,SqlValue)
 item2postgresArrays show' i mm =
-    (toPostgresArray show $ matchPositions mm,toPostgresArray show' $ item2SQLp mm i)
+    (toPostgresArray show . positionalToRanges $ matchPatternToPositional mm
+    , toPostgresArray fromSql (map (toPostgresArray show') (item2SQLp mm i)))
 
-matchPositions :: MatchPattern -> [Int]
-matchPositions (MatchPattern mm) = catMaybes $ zipWith f mm [1..]
-    where f (Just S) x = Just x
-          f (Just L) x = Just $ 6+x
-          f (Just P) x = Just $ 12+x
-          f Nothing  _ = Nothing
+data Range = !Int :-: !Int
+instance Show Range where
+    show (a :-: b) = show a ++ ":" ++ show b
+
+matchPatternToPositional :: MatchPattern -> Positional a
+matchPatternToPositional (MatchPattern mm) = 
+    let (a,b,c) = unzip3 $ map f (take 6 $ mm ++ repeat Nothing) in a++b++c
+    where f (Just P) = (nothing,nothing,Just)
+          f (Just L) = (nothing,Just,nothing)
+          f (Just S) = (Just,nothing,nothing)
+          f Nothing  = (nothing,nothing,nothing)
+          nothing = const Nothing
+
+positionalToRanges :: Positional Int -> [Range]
+positionalToRanges pos = map (\x -> head x :-: last x) . groupByJust $ zipWith ($) pos [1..]
+
+type Positional a = [a -> Maybe a]
 
 toPostgresArray :: (a -> String) -> [a] -> SqlValue
 toPostgresArray show' as = toSql $ '{' : intercalate "," (map show' as) ++ "}"
