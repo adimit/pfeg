f068893d67c491bc1ea396a54312c6f5d01ebccd
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-03-01 21:43:48 +0100

    Remove obsolete stuff from Types

--- src/PFEG/Types.hs
+++ src/PFEG/Types.hs
@@ -2,8 +2,6 @@ module PFEG.Types
     ( Wordcounts
     , Word
     , Sentence
-    , Table
-    , ID
     ) where
 
 import qualified Data.Text as X
@@ -13,6 +11,4 @@ type Wordcounts = T.HashMap X.Text Int
 type Word a = (a,a,a)
 type Sentence a = [Word a]
 
-type Table = String
 
-type ID = Int -- SQL row IDs
