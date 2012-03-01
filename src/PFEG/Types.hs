78d00bd8670db882e22e48426e50523e60537bc9
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-03-01 22:00:29 +0100

    Add parser for matchpatterns.

--- src/PFEG/Types.hs
+++ src/PFEG/Types.hs
@@ -2,17 +2,31 @@ module PFEG.Types
     ( Wordcounts
     , Word
     , Sentence
+    , MatchMode(..)
+    , MatchPattern(..)
+    , parsePattern
     ) where
 
 import qualified Data.Text as X
 import qualified Data.HashMap.Strict as T
+import Text.ParserCombinators.Parsec
 import Data.Bits
 import Data.Hashable
 import Data.List (foldl')
+import Control.Monad (liftM)
 
 type Wordcounts = T.HashMap X.Text Int
 type Word a = (a,a,a)
 type Sentence a = [Word a]
+
+parsePattern :: String -> Either ParseError MatchPattern
+parsePattern = parse patternParser "match pattern"
+
+patternParser :: Parser MatchPattern
+patternParser =
+    let parseLetter c m = char c >> return (Just m)
+        parseNothing    = char '_' >> return Nothing
+    in liftM MatchPattern $ many1 (parseLetter 'L' L <|> parseLetter 'P' P <|> parseLetter 'S' S <|> parseNothing)
 
 data MatchMode = P | L | S deriving (Show,Eq)
 newtype MatchPattern = MatchPattern { unMatchPattern :: [Maybe MatchMode] } deriving (Eq)
