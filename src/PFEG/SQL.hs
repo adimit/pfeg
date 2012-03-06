1ec1916c55fea52e21161c61daf74b67ee815842
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-03-06 23:08:13 +0100

    Simplify schema.

--- src/PFEG/SQL.hs
+++ src/PFEG/SQL.hs
@@ -14,21 +14,15 @@ import qualified Data.Text as T
 import Paths_PFEGtools (version)
 import Data.Version (showVersion)
 
-import Codec.Digest.SHA
-
 insertAction :: String
 insertAction = "INSERT INTO log (action,corpusname,corpusfile,completed,version) VALUES (?,?,?,?,'"
              ++ showVersion version ++ "')"
 
-upsertRecord :: Int -> String
-upsertRecord tnum = "INSERT INTO records (hash,lcs,rcs,lcl,rcl,"++targetSql++") \
-               \VALUES (UNHEX(?),?,?,?,?,1) ON DUPLICATE KEY UPDATE "
-               ++targetSql++" = COALESCE("++targetSql++",0) + 1"
-          where targetSql = 't':show tnum
+upsertRecord :: String
+upsertRecord = "INSERT INTO records (lcs,rcs,lcl,rcl,target) VALUES (?,?,?,?,?)"
 
 
 item2SQL :: Item Text -> [SqlValue]
-item2SQL item@Item { itemLemma = (Context ll rl) , itemSurface = (Context ls rs) } =
-    [ toSql . showBSasHex $ hash SHA256 item
-    , tsql ll, tsql rl, tsql ls, tsql rs ]
+item2SQL Item { itemLemma = (Context ll rl) , itemSurface = (Context ls rs), target = t } =
+    [ tsql ls, tsql rs, tsql ll, tsql rl, toSql t ]
     where tsql = toSql . T.unwords
