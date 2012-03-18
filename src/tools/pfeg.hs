882338c12e153686b16000c97cc1317389a7e882
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-03-19 00:01:05 +0100

    Finish predict task.

--- src/tools/pfeg.hs
+++ src/tools/pfeg.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
 module Main where
 
+import Data.Time.Clock (NominalDiffTime)
 import qualified Text.Search.Sphinx.Types as Sphinx
 import Text.Search.Sphinx.Types (QueryResult(..))
 import Text.Search.Sphinx hiding (sortBy,mode)
 import Data.Binary.Put (Put)
 
+import Data.Maybe (fromMaybe,listToMaybe,catMaybes)
 import qualified Data.ByteString.Lazy.Char8 as B
 import PFEG
 import PFEG.Types
@@ -70,45 +72,125 @@ process session =
           mvar <- newEmptyMVar
           cmd <- newMVar ()
           void $ forkIO . forever $ recorder s mvar cmd
-          let it = standardIteratee (recordF mvar) session
+          let it = standardIteratee (getItems $ targets session) (recordF mvar)
           void $ workOnCorpora it session (0,[]) cs
           putStrLn "Waiting for DB…" >> takeMVar cmd
         Match{ corpora = cs, resultLog = l } -> do
           chan <- newChan
-          void $ forkIO (evalStateT (matchLogger l chan) initialMatcherState)
-          let it = standardIteratee (matchF chan) session
+          void $ forkIO (evalPFEG (matchLogger l chan) initialMatchScore session)
+          let it = standardIteratee (getItems $ targets session) (matchF chan)
           void $ workOnCorpora it session () cs
-        Predict { } -> undefined
+        Predict { corpora = cs, resultLog = l } -> do
+          chan <- newChan
+          void $ forkIO (evalPFEG (matchLogger l chan) initialPredictScore session)
+          let it = standardIteratee (getMaskedItems' $ targets session) (matchF chan)
+          void $ workOnCorpora it session () cs
 
-matchLogger :: Handle -> Chan (Item Text, Sphinx.Result [QueryResult]) -> StateT MatcherState IO ()
-matchLogger l c = do
-    next
-    (item,result) <- liftIO $ readChan c
-    results <- case result of
-         (Sphinx.Warning warn a) -> liftIO (putStrLn $ "WARNING: " ++ B.unpack warn) >> return a
+matchF :: QueryChan -> ItemProcessor_
+matchF log item = do
+    session <- ask
+    queries <- mapM (makeAQuery item) patterns
+    (results,time) <- liftIO . doTimed $ runQueries (searchConf.pfegMode $ session) queries
+    liftIO $ writeChan log (item,results,time)
+
+type QueryChan = Chan (Item Text, Sphinx.Result [QueryResult],NominalDiffTime)
+
+initialPredictScore, initialMatchScore :: Score
+initialPredictScore = PredictScore 0 0 0 0 0 0 0 0 0 0
+initialMatchScore = MatchScore 0 0
+
+tickScore :: (MonadState Score m) => m ()
+tickScore = modify' $ \ s -> s { totalScored = totalScored s+1 }
+
+score :: Score -> Token Text -> [Prediction] -> PFEG a (Score,SphinxPattern,Text)
+score s Masked { original = orig, surface = sfc, alternatives = alts } ps = do
+    (bestPrediction,pattern) <- findBestPrediction ps
+    return (whichCase s bestPrediction sfc orig alts,pattern,bestPrediction)
+score s Word { surface = sfc } ps = do
+    (bestPrediction,pattern) <- findBestPrediction ps
+    return (whichCase s bestPrediction sfc T.empty [],pattern,bestPrediction)
+
+whichCase :: Score -> Text -> Text -> Text -> [Text] -> Score
+whichCase s@MatchScore { } p gold _orig alts
+     | p == gold || p `elem` alts = s { scoreCorrect = scoreCorrect s + 1 }
+     | otherwise = s
+whichCase s@PredictScore { } p gold orig alts
+     | p == gold && p == orig = s { scoreAAA = scoreAAA s + 1, scoreCorrect = scoreCorrect s + 1 }
+     | p == gold             = s { scoreAAB = scoreAAB s + 1, scoreCorrect = scoreCorrect s + 1 }
+     | p == orig             = s { scoreABA = scoreABA s + 1 }
+     | orig == gold          = s { scoreABB = scoreABB s + 1 }
+     | p `elem` alts && p == orig = s { scoreAAAContained = scoreAAAContained s + 1
+                                      , scoreCorrect      = scoreCorrect s      + 1 }
+     | p `elem` alts              = s { scoreAABContained = scoreAABContained s + 1
+                                      , scoreCorrect      = scoreCorrect s      + 1 }
+     | orig `elem` alts           = s { scoreABBContained = scoreABBContained s + 1 }
+     | otherwise = s { scoreABC = scoreABC s + 1 }
+
+findBestPrediction :: [Prediction] -> PFEG a (Text,SphinxPattern)
+findBestPrediction ps = do
+    def <- liftM (T.pack . majorityBaseline) ask
+    return $ fromMaybe (def, MajorityBaseline) (listToMaybe . catMaybes $ zipWith f (map listToMaybe ps) patterns)
+    where f Nothing _ = Nothing
+          f (Just (x,_))  p = Just (x,p)
+
+getQueryResults :: Sphinx.Result [QueryResult] -> IO [QueryResult]
+getQueryResults result =
+    case result of
+         (Sphinx.Warning warn a) -> putStrLn ("WARNING: " ++ B.unpack warn) >> return a
          (Sphinx.Ok a)           -> return a
-         (Sphinx.Error code msg) -> liftIO (putStrLn $ "ERROR ("++ show code++"): " ++ B.unpack msg) >> return []
-         (Sphinx.Retry msg)      -> liftIO (putStrLn $ "RETRY: " ++ B.unpack msg) >> return []
-    let preds = zip (map parseResult results) patterns
-        log' = uncurry $ logDataLine item
-    ls <- mapM log' preds
-    liftIO $ forM_ ls (\line -> hPutStr l line >> hFlush l)
+         (Sphinx.Error code msg) -> putStrLn ("ERROR ("++ show code++"): " ++ B.unpack msg) >> return []
+         (Sphinx.Retry msg)      -> putStrLn ("RETRY: " ++ B.unpack msg) >> return []
 
-logDataLine :: Item Text -> Prediction -> SphinxPattern -> StateT MatcherState IO String
+matchLogger :: Handle -> QueryChan -> PFEG Score ()
+matchLogger l c = do
+    tickScore
+    (item,result,time) <- liftIO $ readChan c
+    results <- liftIO $ getQueryResults result
+    currentScore <- get
+    let preds = zip (map parseResult results) patterns
+    (newScore,winningPattern,bestPrediction) <- score currentScore (target item) (map fst preds)
+    put $! newScore
+    let log' = uncurry $ logDataLine item time
+    ls <- mapM log' preds
+    liftIO $ forM_ ls (\line -> hPutStrLn l line >> hFlush l)
+    liftIO . putStrLn $ "P: " ++ T.unpack bestPrediction ++
+                      "\nA: " ++ show (target item) ++
+                      "\nT: " ++ renderS time ++
+                      "\nS: " ++ show newScore ++
+                      "\nX: " ++ show winningPattern
+    matchLogger l c
+
+scoreboard :: Score -> [String]
+scoreboard s@MatchScore { }   = map show (zipWith ($) [totalScored, scoreCorrect] (repeat s))
+scoreboard s@PredictScore { } = map show (zipWith ($) ([totalScored, scoreCorrect] ++ canonicalPredictScore) 
+                                                       (repeat s))
+
+canonicalPredictScore :: [Score -> Int]
+canonicalPredictScore = [ scoreAAA
+                        , scoreAAB
+                        , scoreABA
+                        , scoreABB
+                        , scoreABC
+                        , scoreAAAContained
+                        , scoreAABContained
+                        , scoreABBContained ]
+
+logDataLine :: Item Text -> NominalDiffTime -> Prediction -> SphinxPattern -> PFEG Score String
 logDataLine Item { itemLemma = Context lcl rcl
                  , itemSurface = Context lcs rcs
-                 , target = w } ps p = do
-    (MatcherState x _) <- get
+                 , target = w } time ps p = do
+    x <- liftM totalScored get
     return $ intercalate "\t" $
         [ show x                        -- item number
         , T.unpack . surface $ w        -- actually there
         , show p ]                      -- pattern
         ++ predictions ps               -- our predictions
         ++ map untext [lcl,rcl,lcs,rcs] -- the item
+        ++ [ renderS time ]             -- the time the query took
 
 -- first three predictions
 predictions :: Prediction -> [String]
-predictions ps = take 3 $ concatMap (\ (a,b) -> [T.unpack a,show b] ) ps ++ repeat ""
+predictions ps = take 6 $ concatMap (\ (a,b) -> [T.unpack a,show b] ) ps ++ repeat ""
 
 untext :: [Text] -> String
 untext = T.unpack . T.unwords
@@ -117,22 +199,27 @@ next, correct :: MonadState MatcherState m => m ()
 next    = modify' $ \ (MatcherState t c) -> MatcherState (t+1) c
 correct = modify' $ \ (MatcherState t c) -> MatcherState t (c+1)
 
-data Score = Score
-    { totalScored       :: !Int
-    , scoreAAA          :: !Int
-    , scoreAAB          :: !Int
-    , scoreABA          :: !Int
-    , scoreABB          :: !Int
-    , scoreABC          :: !Int
-    , scoreAAAContained :: !Int
-    , scoreAABContained :: !Int
-    } deriving Show
+data Score = PredictScore { totalScored       :: !Int
+                          , scoreAAA          :: !Int
+                          , scoreAAB          :: !Int
+                          , scoreABA          :: !Int
+                          , scoreABB          :: !Int
+                          , scoreABC          :: !Int
+                          , scoreAAAContained :: !Int
+                          , scoreAABContained :: !Int
+                          , scoreABBContained :: !Int
+                          , scoreCorrect :: !Int }
+           | MatchScore   { totalScored  :: !Int
+                          , scoreCorrect :: !Int }
 
-{-score :: Score -> Token Text -> Prediction -> Score
-score s Masked { original = orig, surface = sfc, alternatives = alts } ps = undefined
-
-nullScore :: Score
-nullScore = undefined -}
+instance Show Score where
+    show s = '(':show (scoreCorrect s) ++ "/" ++ show (totalScored s) ++ ") " ++
+             case s of
+                  MatchScore { } -> ""
+                  PredictScore { } -> intercalate ", " $ zipWith f 
+                                      ["aaa","aab","aba","abb","abc","aaaC","aabC","abbC"]
+                                      canonicalPredictScore
+                        where f name scr = name ++ ": " ++ show (scr s)
 
 type Prediction = [(Text,Int)]
 data MatcherState = MatcherState { totalMatches :: !Int, correctMatches :: !Int }
@@ -152,15 +239,9 @@ correctPrediction :: (MonadState MatcherState m) => m ()
 correctPrediction = modify' (\MatcherState { totalMatches = t, correctMatches = c } ->
                               MatcherState (t+1) (c+1))
 
-matchF :: Chan (Item Text, Sphinx.Result [QueryResult]) -> ItemProcessor_
-matchF log item = do
-    session <- ask
-    queries <- mapM (makeAQuery item) patterns
-    results <- liftIO $ runQueries (searchConf.pfegMode $ session) queries
-    liftIO $ writeChan log (item,results)
-
 data SphinxPattern = Surface { width :: !Int, tolerance :: !Int }
                    | Lemma   { width :: !Int, tolerance :: !Int }
+                   | MajorityBaseline
 
 -- TODO: add a Parser SphinxPattern so we can put patterns in the config
 
@@ -179,6 +260,7 @@ testItem  = Item { itemSurface = Context ["ich","gehe"] ["die","uni"]
 getLetter :: SphinxPattern -> Char
 getLetter Surface {} = 's'
 getLetter Lemma   {} = 'l'
+getLetter MajorityBaseline = 'M'
 
 makeQuery :: Item Text -> SphinxPattern -> String
 makeQuery i p =
@@ -193,6 +275,8 @@ makeQuery i p =
 getContext :: Item a -> SphinxPattern -> ([a],[a])
 getContext Item { itemSurface = (Context ls rs) } (Surface {width = w}) = makeContext ls rs w
 getContext Item { itemLemma   = (Context ls rs) } (Lemma {width = w})   = makeContext ls rs w
+getContext _ MajorityBaseline = error "We don't expect to get the context of a majority baseline pattern!"
+
 makeContext :: [a] -> [a] -> Int -> ([a],[a])
 makeContext ls rs i = (reverse $ take i (reverse ls),take i rs)
 
@@ -203,7 +287,7 @@ makeAQuery item pattern = do
     return $ addQuery conf q "*" (show pattern)
 
 patterns :: [SphinxPattern]
-patterns = [ Surface x y | x <- [3,2,1], y <- [0..2] ] ++ [ Lemma x y | x <- [3,2] , y <-[0..1] ]
+patterns = [ Surface x y | x <- [4,3,2,1], y <- [0,1,2] ] ++ [ Lemma x y | x <- [4,3,2,1] , y <- [0,1,2] ]
 
 workOnCorpora :: I.Iteratee (Sentence Text) (PFEG st) () -> PFEGConfig -> st -> [Corpus] -> IO [st]
 workOnCorpora it session st = mapM $ \ c@(_cName,cFile) -> do
@@ -231,8 +315,8 @@ recordF mvar item = do
        then put (0,[]) >> liftIO (putMVar mvar vals')
        else put (i+1,vals')
 
-standardIteratee :: ItemProcessor st -> PFEGConfig -> Iteratee (Sentence Text) (PFEG st) ()
-standardIteratee proc session = I.mapChunksM_ $ mapM proc . getItems (targets session)
+standardIteratee :: ItemGetter -> ItemProcessor st -> Iteratee (Sentence Text) (PFEG st) ()
+standardIteratee gI proc = I.mapChunksM_ $ mapM proc . gI
 
 type ItemProcessor st = Item Text -> PFEG st ()
 type ItemProcessor_ = ItemProcessor ()
