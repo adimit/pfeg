d78467102bf033f4c46e24a8016d1489f115c2ba
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-06-23 00:46:47 +0200

    Largely rewrite PFEG.Pattern
    It's less capable now, but that's ok. We don't need it to be
    super-powerful. Just fucking work already.

--- src/PFEG/Pattern.hs
+++ src/PFEG/Pattern.hs
@@ -1,48 +1,105 @@
-module PFEG.Pattern where
+{-# LANGUAGE OverloadedStrings #-}
+module PFEG.Pattern
+    ( -- * Pattern data types
+      -- | Types for patterns inside @PFEG@
+      MatchPattern(..)
+    , Match(..)
+    , Level(..)
+    , MatchData(..)
+    , Interference(..)
+      -- ** Parsing and rendering functions for patterns
+    , parsePattern
+    , renderAsSphinx
+    , matchParser
+    ) where
 
+import PFEG.Types
+import PFEG.ShortestMatch
 import Text.Parsec.Text
-import Data.Text.ICU (Regex)
 import Data.Text (Text)
 import Text.Parsec.Char
 import Text.Parsec.Prim
 import Text.Parsec.Combinator
+import PFEG.Context (Item,Context)
+import qualified PFEG.Context as C
+import Control.Monad (liftM)
+import qualified Data.Text as T
+
+data MatchData = MatchData
+    { predictedTarget  :: Token Text
+    , interferingWords :: Interference (Token Text)
+    } deriving Show
 
 data MatchPattern = MatchPattern { left  :: Match
                                  , right :: Match
-                                 , centerInterference  :: Int }
+                                 , centerInterference  :: Int
+                                 , level :: Level}
 
-data Match = Match { form :: [Level], tolerance :: Int }
+data Match = Match { size :: Int, tolerance :: Int }
 
 data Level = Surface | Lemma
 
 instance Show Level where
-    show Surface = "s"
-    show Lemma   = "l"
+    show Surface = "S"
+    show Lemma   = "L"
 
 instance Show Match where
-    show Match { form = levels, tolerance = t } = foldl (\a l -> a ++ show l) "" levels ++ show t
+    show Match { size = s, tolerance = t } = show s ++ '~':show t
 
 instance Show MatchPattern where
-    show mp = show (left mp) ++ '-':show (centerInterference mp) ++ '-':show (right mp)
+    show mp = show (level mp) ++ show (left mp) ++ '-':show (centerInterference mp) ++ '-':show (right mp)
 
 parseLevel :: Parser Level
-parseLevel = (char 'l' >> return Lemma) <|> (char 's' >> return Surface)
+parseLevel = (char 'S' >> return Lemma) <|> (char 'L' >> return Surface)
 
 parseMatch :: Parser Match
 parseMatch = do
-    lvls <- many1 parseLevel
-    tol  <- many1 digit
-    return Match { form = lvls, tolerance = read tol }
+    sz  <- liftM read $ many1 digit
+    tol <- option 1 (try (char '~' >> liftM read (many1 digit)))
+    return Match { size = sz, tolerance = tol }
 
 parsePattern :: Parser MatchPattern
 parsePattern = do
+    lvl <- parseLevel
     l <- parseMatch
     inter <- char '-' >> many1 digit
     r     <- char '-' >> parseMatch
-    return MatchPattern { left = l, right = r, centerInterference = read inter }
+    return MatchPattern { level = lvl, left = l, right = r, centerInterference = read inter }
 
-renderAsSphinx :: MatchPattern -> Text
-renderAsSphinx = undefined
+-- | Given an item i and pattern p, construct a query for the Sphinx server
+-- that looks for something similar to i as defined by p.
+renderAsSphinx :: C.Item Text -> MatchPattern -> Text
+renderAsSphinx i p =
+    let (lwords,rwords) = retrieveWords i p
+    in T.concat [ pr lwords (left p)
+                , T.pack ('-':show (centerInterference p))
+                , pr rwords (right p) ]
+    where pr ws m = T.concat [ wrap '"' . T.intercalate " " $ renderLevel (level p):ws
+                             , renderTolerance (tolerance m) ]
 
-renderAsRegex :: MatchPattern -> Regex
-renderAsRegex = undefined
+matchParser :: [Text] -> Context (Token Text) -> Sentence Text -> [MatchData]
+matchParser targets cxt s =
+    let prediction = findTarget ((`elem` targets).surface) (C.left cxt, C.right cxt) s
+    in  maybe [] pred2match prediction
+
+pred2match :: Prediction (Token Text) -> [MatchData]
+pred2match (Prediction possT f_inter) = map (\t -> MatchData t (f_inter t)) possT
+
+renderLevel :: Level -> Text
+renderLevel Surface = "@surface"
+renderLevel Lemma   = "@lemma"
+
+chooseLevel :: Level -> Item a -> Context a
+chooseLevel Surface = C.itemSurface
+chooseLevel Lemma   = C.itemLemma
+
+wrap :: Char -> Text -> Text
+wrap c x = T.cons c $ T.concat [x, T.singleton c]
+
+renderTolerance :: Int -> Text
+renderTolerance t = if t > 0 then T.pack ('~':show t) else T.empty
+
+retrieveWords :: Item a -> MatchPattern -> ([a],[a])
+retrieveWords i MatchPattern { left = l, right = r, level = lvl } =
+    ( reverse . take (size l) . reverse . C.left  . chooseLevel lvl $ i
+    ,           take (size r)           . C.right . chooseLevel lvl $ i )
