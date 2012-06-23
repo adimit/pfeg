3f5ce0cf4b587b48c3e754a9ef338ea9e30ec394
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-06-24 01:18:51 +0200

    Remove all tagging shenanigans from Context.

--- src/PFEG/Context.hs
+++ src/PFEG/Context.hs
@@ -11,55 +11,21 @@ module PFEG.Context
     , getItems
     , getMaskedItems
     , getMaskedItems'
-    , tagSentence
     ) where
 
-import PFEG
-import PFEG.Configuration
 import PFEG.Types
 
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Encoding
 
-import Data.List (findIndices,mapAccumL)
+import Data.List (findIndices)
 
 import Data.Traversable (Traversable)
 import Data.Foldable (Foldable)
 
 import Control.Monad.Reader
 import Codec.Digest.SHA.Monad
-
-o_targetTag, c_targetTag :: Token Text
-o_targetTag = nonWord "<t>"
-c_targetTag = nonWord "</t>"
-
--- | Insert tags around items in a list at given indices. Indices are 0-based
--- and *must* be sorted.
--- CAREFUL: If the tagged @Token@s are anything but @Word@, they will not
--- retain that property, but they will be turned into @Word@ upon tagging, even
--- if they use, say @Masked@.
-insertTags :: [Token Text] -- ^ The list to insert tags into
-             -> (Token Text,Token Text) -- ^ The open (fst) and close (snd) tags
-             -> [Int] -- ^ *Sorted* list of 0-based indices at which to insert tags.
-             -> [Token Text]
--- maybe this should've been implemented with iterate instead?
-insertTags s (open,close) idx =
-    snd $ mapAccumL f idx (zip s [0..])
-    where f []            (stoken,_)                    = ([], stoken)
-          f idx'@(i:idxs) (stoken,sindex) | i == sindex = (idxs, merge [open,stoken,close])
-                                          | otherwise   = (idx', stoken)
-          -- CAREFUL: This will squish any kind of Token into a Word
-          merge :: [Token Text] -> Token Text
-          merge t = Word { surface = T.concat . fmap surface $ t
-                         , lemma   = T.concat . fmap lemma   $ t
-                         , pos     = T.concat . fmap pos     $ t }
-
-tagSentence :: Sentence Text -> PFEG st (Sentence Text)
-tagSentence s = do
-    ts <- liftM targets ask
-    let target_indices = findIndices ((`elem` ts).surface) s
-    return $ insertTags s (o_targetTag, c_targetTag) target_indices
 
 data Context a = Context { left  :: ![a]
                          , right :: ![a]
