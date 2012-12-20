7c9a6cfb1ead23e99a14b08adcaaac4e9d0b24e2
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2012-12-20 02:21:53 +0100

    Make graphs prettier.

--- src/analyze.R
+++ src/analyze.R
@@ -2,7 +2,7 @@ library(plyr)
 library(reshape2)
 library(ggplot2)
 
-prefix <- "de"
+prefix <- "en"
 name <- function(x) { paste(prefix,x,sep="_") }
 filename <- name("pfeg-stats.csv")
 
@@ -32,11 +32,27 @@ targets <- dcast(dcast(pfeg, itemID + target ~ .)[,c(1,2)], target ~ .)
 colnames(targets) <- c("target","count")
 baseline = max(targets$count) / sum(targets$count)
 
+selected_patterns = c("L0-3~1","L3-0~1",
+                      "S0-3~1","S3-0~1",
+                      "S0-4~1","S4-0~1",
+                      "L0-4~1","L4-0~1",
+                      "S0-5~1","S5-0~1",
+                      "L0-5~1","L5-0~1",
+                      "S0-2~1","S2-0~1",
+                      "L0-2~1","L2-0~1")
+
+pfeg.stats.small <- pfeg.stats[pfeg.stats$pattern %in% selected_patterns,]
+pfeg.stats$tolerance <- as.factor(substr(pfeg.stats$pattern,6,6))
+pfeg.stats$level <- as.factor(substr(pfeg.stats$pattern,0,1))
+
 # Accuracy/Coverage graph
-acccov <- ggplot(pfeg.stats,aes(accuracy,coverage,label=pattern)) + geom_text(hjust=0,vjust=0,size=8) + geom_point() +
+acccov <- ggplot(pfeg.stats,aes(accuracy,coverage)) + geom_point(size=I(5),aes(shape=level,color=tolerance)) +
+	geom_text(hjust=0,vjust=0,size=8,data=pfeg.stats.small,aes(accuracy,coverage,label=pattern)) +
 	labs(x="Accuracy", y="Coverage") + expand_limits(x=c(0.2,1),y=c(0.2,1)) +
 	geom_smooth(method="lm",formula=y ~ poly(x,3,raw=T)) +
 	annotate("segment", x=baseline,xend=baseline,y=0.1,yend=1,colour="red") +
+	scale_shape_discrete(name="Index:", labels=c("Lemma","Surface")) +
+	scale_colour_discrete(name="Tolerance:") +
 	annotate("text", x=baseline+0.06,y=0.4,label="baseline", colour="red",size=10)
 
 theme1 <- theme_bw() + theme(axis.text = element_text(size=14),text = element_text(size=26),legend.position="bottom")
@@ -77,7 +93,7 @@ printConvergenceGraphs <- function(convg,stepsize=100) {
         dev.off()
 }
 
-printConvergenceGraphs(makeConvergence())
+#printConvergenceGraphs(makeConvergence())
 
 meanQueryTime <- sum(dcast(pfeg[,c("itemID","queryTime")],itemID ~ ., fun.aggregate=mean,value.var="queryTime")[,2])/length(pfeg$queryTime)
 writeLines(paste("Mean cohort query execution time:",meanQueryTime,"seconds"))
